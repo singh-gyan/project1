f6e58ca1f582110dde21b5763eb25777a7ee8c88
Abhigyan <singhabhigyan97@gmail.com>
2022-08-27 13:25:33 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import { Stack } from '@mui/material';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div style={{ justifyContent: 'center', alignItems: 'center' }}></div>;
+}
+
+export default App;
